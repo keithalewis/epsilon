e532e0ed5171505b83c7158bb24a4514aa4e18d5
kal <kal@kalx.net>
2019-08-21 12:51:48 -0400

    Put in test for dereferencable sequence

--- bell.h
+++ bell.h
@@ -17,19 +17,20 @@ namespace fms {
             B.push_back(1);
         }
         X operator[](size_t n) {
-            if (n < B.size())
-                return B[n];
-            for (size_t k = B.size(); k <= n; ++k) {
-                X Bk = 0;
-                X Ckj = 1; // really C(k-1,j)
-                S ak = a;
-                for (size_t j = 0; j < k /*&& ak*/; ++j) {
-                    Bk += Ckj * operator[](k - 1 - j) * (*ak);
-                    Ckj *= k - 1 - j;
-                    Ckj /= j + 1;
-                    ++ak;
+
+            if (n >= B.size()) {
+                for (size_t k = B.size(); k <= n; ++k) {
+                    X Bk = 0;
+                    X Ckj = 1; // really C(k-1,j)
+                    S ak = a;
+                    for (size_t j = 0; j < k && ak; ++j) {
+                        Bk += Ckj * operator[](k - 1 - j) * (*ak);
+                        Ckj *= k - 1 - j;
+                        Ckj /= j + 1;
+                        ++ak;
+                    }
+                    B.push_back(Bk);
                 }
-                B.push_back(Bk);
             }
 
             return B[n];
