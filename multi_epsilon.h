29f1142b528f5fbb03bf461fd71b1763f7b470dd
tianling shen <schoolfield@126.com>
2020-01-24 12:04:50 -0500

    new implementation of mult_epsilon pass all tests

--- multi_epsilon.h
+++ multi_epsilon.h
@@ -12,25 +12,28 @@ using std::vector;
 namespace fms {
 	class multi_epsilon {
 	public:
-		intptr_t  Size;
+		intptr_t m;//how many variables
+		intptr_t Size;//size of m_lpBuf
 		intptr_t N;//N=2 if only consider 1st order derivative
-		//!! needs more thoughts on how to implement this
+		//!! needs more ideas on how to implement this
 		double zero = 0;
 
 		//m variable,  derivatives up to order n
 		multi_epsilon(intptr_t m = 0, intptr_t n=0)
-			: Size(m), m_lpBuf(0.0, pow(n+1,m)), N(n+1) {};
+			: Size(pow(n + 1, m)), m_lpBuf(0.0, pow(n+1,m)), N(n+1), m(m) {};
 
 		multi_epsilon(const multi_epsilon& rhs)
-			:Size(rhs.Size), m_lpBuf(rhs.m_lpBuf), N(rhs.N)
+			:Size(rhs.Size), m_lpBuf(rhs.m_lpBuf), N(rhs.N), m(rhs.m)
 		{};
 
 		
-		multi_epsilon(const std::initializer_list<double>& rhs, intptr_t n)
-			:m_lpBuf(rhs), Size(rhs.size()), N(n+1) { };
+		multi_epsilon(const std::initializer_list<double>& rhs, intptr_t m, intptr_t n)
+			:m_lpBuf(rhs), Size(rhs.size()), N(n+1), m(m) { };
 
 		multi_epsilon(multi_epsilon&& rhs) noexcept {
 			this->Size = rhs.Size;
+			this->N = rhs.N;
+			this->m = rhs.m;
 			this->m_lpBuf = std::move(rhs.m_lpBuf);
 		};
 
@@ -42,6 +45,8 @@ namespace fms {
 		multi_epsilon& operator=(const multi_epsilon& rhs) {
 			this->Size = rhs.Size;
 			this->m_lpBuf = rhs.m_lpBuf;
+			this->N = rhs.N;
+			this->m = rhs.m;
 			return *this;
 		};
 
@@ -49,6 +54,8 @@ namespace fms {
 		multi_epsilon& operator=(multi_epsilon&& rhs) noexcept {
 			this->Size = rhs.Size;
 			this->m_lpBuf = std::move(rhs.m_lpBuf);
+			this->N = rhs.N;
+			this->m = rhs.m;
 			return *this;
 		};
 
@@ -56,6 +63,8 @@ namespace fms {
 
 		bool operator == (const multi_epsilon& rhs) const {
 			if (this->Size != rhs.Size) return false;
+			if (this->m != rhs.m) return false;
+			if (this->N != rhs.N) return false;
 			return (m_lpBuf == rhs.m_lpBuf).min() == true;
 		}
 
@@ -66,6 +75,8 @@ namespace fms {
 		multi_epsilon& operator += (const multi_epsilon& rhs)
 		{
 			assert(rhs.Size == this->Size);
+			assert(rhs.m == this->m);
+			assert(rhs.N == this->N);
 			m_lpBuf += rhs.m_lpBuf;
 			return *this;
 		}
@@ -80,6 +91,8 @@ namespace fms {
 		multi_epsilon& operator -= (const multi_epsilon& rhs)
 		{
 			assert(rhs.Size == this->Size);
+			assert(rhs.m == this->m);
+			assert(rhs.N == this->N);
 			m_lpBuf -= rhs.m_lpBuf;
 			return *this;
 		}
@@ -97,6 +110,8 @@ namespace fms {
 		{
 			//if (!rhs.m_lpBuf) return *this;
 			assert(rhs.Size == this->Size);
+			assert(rhs.m == this->m);
+			assert(rhs.N == this->N);
 			std::valarray<double> temp((double)0, this->Size);
 			intptr_t i = 0;
 			{
@@ -126,6 +141,8 @@ namespace fms {
 		{
 			//if (!rhs.m_lpBuf) return *this;
 			assert(rhs.Size == this->Size);
+			assert(rhs.m == this->m);
+			assert(rhs.N == this->N);
 			multi_epsilon rhs_inv = rhs.inverse();
 			operator*=(rhs_inv);
 			return *this;
@@ -154,7 +171,7 @@ namespace fms {
 			intptr_t k = N;
 			while (k <= Size) {
 				if ((i % k) > (j % k))
-					return 0;
+					return zero;
 				k *= N;
 			}
 			return m_lpBuf[j - i];
@@ -186,7 +203,7 @@ namespace fms {
 		//inverse(this)
 		//Gauss reduction method
 		multi_epsilon inverse() const {
-			multi_epsilon res(Size, N-1);
+			multi_epsilon res(m, N-1);
 			res[0] = 1.0 / m_lpBuf[0];
 			for (intptr_t i = 1; i < Size; i++) {
 				double cum_prod = 0;
@@ -210,26 +227,25 @@ namespace fms {
 
 
 		//epsilon \otimes epsilon \otimes I
-		//= 2*2*1 -1
 		//return 3
-		static intptr_t rep(const std::vector<intptr_t>& order) {
-			intptr_t res=1;
-			for (intptr_t i = 0; i < order.size(); i++) {
-				res *= (order[i] + 1);
+		static intptr_t rep(const std::vector<intptr_t>& order, const intptr_t n) {
+			intptr_t res=order[0]+1;
+			for (intptr_t i = 1; i < order.size(); i++) {
+				res = (n+1)*(res-1)+order[i]+1;
 			}
 			return res - 1;
 		}
 
-		static vector<multi_epsilon> add_epsilon(const vector<double>& x, intptr_t n) {
+		static vector<multi_epsilon> add_epsilon(const vector<double>& x, const intptr_t n) {
 			vector<multi_epsilon> result;
-			vector<intptr_t> order(0.0,x.size());
+			vector<intptr_t> order(x.size(), 0);
 			for (intptr_t i = 0; i < x.size(); i++) {
-				order[i] = 1.0;
+				order[i] = 1;
 				multi_epsilon temp = identity(x.size(), n);
 				temp *= x[i];
-				temp[rep(order)] = 1.0;
-				result.emplace_back(std::move(temp));
-				order[i] = 0.0;
+				temp[rep(order, n)] = 1.0;
+				result.emplace_back(temp);
+				order[i] = 0;
 			}
 			return result;
 		}
